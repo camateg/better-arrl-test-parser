d9266703b9755a3ff9c799bba4eaac9b002ec7f7
Matthew Fleeger <matt@Matthews-Mac-mini.local>
2015-09-11 15:53:49 -0400

    Stuff.

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 var express = require('express')
    ,cheerio = require('cheerio')
+   ,path = require('path')
+   ,jade = require('jade')
    ,fs = require('fs');
 
 var app = express();
 var port = process.env.PORT || 5000;
+app.use("/public", express.static(path.join(__dirname, 'public')));
+app.set('views', './views');
+app.set('view engine', 'jade');
+
 app.listen(port);
 
 app.get('/:type/json', init_exam, all_tests, json_out);
 app.get('/:type/rand/json', init_exam, random_test, json_out);
+app.get('/:type/:q/json', init_exam, specific_test, json_out);
+
 app.get('/', set_tech, random_test, out);
 
 app.get('/:type/rand', init_exam, random_test, out);
+app.get('/:type/:q', init_exam, specific_test, single_out);
 
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -34,6 +43,25 @@ function init_exam(req, res, next) {
   return next();
 }
 
+function specific_test(req, res, next) {
+  res.stash = res.stash || {};
+  res.stash.type = req.params.type;
+  res.stash.q = req.params.q;
+
+  res.stash.exam = require('./data/' + res.stash.type + '.json');
+
+  res.stash.output = {};
+
+  for(q in res.stash.exam) {
+     console.log(res.stash.exam[q]);
+     if(res.stash.q === res.stash.exam[q].question_no) {
+       res.stash.output = res.stash.exam[q];
+     }
+  }
+
+  return next(); 
+}
+
 function random_test(req, res, next) {
   res.stash = res.stash || {};
 
@@ -43,6 +71,8 @@ function random_test(req, res, next) {
 
   return next();
 }
+
+
 
 function all_tests(req, res, next) {
   res.stash = res.stash || {};
@@ -63,11 +93,17 @@ function json_out(req, res) {
 function out(req, res) {
   res.stash = res.stash || {};
 
-  fs.readFile('views/one.html', function(err, data) {
+  fs.readFile('views/rand.html', function(err, data) {
     if (err) {
       console.log(err);
     }
     res.write(data);
     res.end();
   }); 
+}
+
+function single_out(req, res) {
+  res.stash = res.stash || {};
+
+  res.render('one', {data: {class: res.stash.type, question: res.stash.q }});
 }
